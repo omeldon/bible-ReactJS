586a80912d11795cad63c5c26691721b37a4eb79
Rommel Dones <donesrommel2000@gmail.com>
2025-08-21 00:33:03 +0800

    Initial commit - pushed React project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import QuotesApp from './components/QuotesApp';
+
+function App() {
+  return (
+    <div className="App">
+      <QuotesApp />
+    </div>
+  );
+}
+
+export default App;
